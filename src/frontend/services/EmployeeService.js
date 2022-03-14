4e14d6b5ca6cf84eb80af190d80849d8bbe1e43f
shireen <syed.shireen@everest.engineering>
2022-03-14 21:20:16 +0530

    Create new employee ui designed.

--- src/frontend/services/EmployeeService.js
+++ src/frontend/services/EmployeeService.js
@@ -6,3 +6,19 @@ export function getAllEmployees() {
   return axios.get(`${API_BASE_URL}/api/employees`);
 }
 
+export function createEmployee(employee) {
+  return axios.post(`${API_BASE_URL}/api/employees`, employee);
+}
+export function getEmployeeById(id) {
+  return axios.get(`${API_BASE_URL}/api/employees/${id}`);
+}
+export function getEmployeeByFirstNameOrLastName(empName) {
+  return axios.get(`${API_BASE_URL}/api/employees/search?query=${empName}`);
+}
+
+export function updateEmployee(id, employee) {
+  return axios.put(`${API_BASE_URL}/api/employees/${id}`, employee);
+}
+export function deleteEmployee(id) {
+  return axios.delete(`${API_BASE_URL}/api/employees/${id}`);
+}
