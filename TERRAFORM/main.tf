a5e7977b7c10db5a465c54ac9339ee944b39132a
ShireenAzad <syed.shireen@everest.engineering>
2022-05-17 15:51:29 +0530

    terraform provider removed.

---
+++ TERRAFORM/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region              = var.aws_region
+  shared_config_files = ["/Users/shireenazad/.aws/config"]
+  profile             = "shireen_syed"
+}
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "employeeportalsecretkey" {
+  key_name   = "employeeportalsecretkey" # Create a "myKey" to AWS!!
+  public_key = file("/Users/shireenazad/.ssh/id_rsa.pub")
+
+  provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./employeeportalsecretkey.pem"
+  }
+}
+
+#Create security group with firewall rules
+resource "aws_security_group" "security_port" {
+  name        = "security_port"
+  description = "security group "
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "security_port"
+  }
+}
+
+resource "aws_instance" "employeePortal" {
+  ami             = "ami-0756a1c858554433e"
+  key_name        ="employeeportalsecretkey"
+  instance_type   = var.instance_type
+  security_groups = ["security_port"]
+  tags = {
+    Name = "employeeportal_ec2_instance"
+  }
+}
+
