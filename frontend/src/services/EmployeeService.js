2caa1011e9ceee8d96bee75f4eec124528551622
ShireenAzad <88663880+ShireenAzad@users.noreply.github.com>
2022-05-25 20:46:39 +0530

    Update EmployeeService.js

--- frontend/src/services/EmployeeService.js
+++ frontend/src/services/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://"+process.env.REACT_APP_IP_ADDRESS+":8080";
-console.log(process.env.REACT_APP_IP_ADDRESS)
+const API_BASE_URL =   `http://${process.env.REACT_APP_IP}:8080`;
+console.log(process.env.REACT_APP_IP)
 export function getAllEmployees() {
   return axios.get(`${API_BASE_URL}/api/employees`);
 }
