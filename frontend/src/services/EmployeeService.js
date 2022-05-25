e12bd7bddf990a3b6f0bc140c470457d357ccf11
ShireenAzad <88663880+ShireenAzad@users.noreply.github.com>
2022-05-25 23:26:47 +0530

    Update EmployeeService.js

--- frontend/src/services/EmployeeService.js
+++ frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =  process.env.REACT_APP_BASE_URL
+const API_BASE_URL =  "http://"+process.env.REACT_APP_BASE_URL+":8080"
 console.log(process.env.REACT_APP_BASE_URL)
 export function getAllEmployees() {
   return axios.get(`${API_BASE_URL}/api/employees`);
